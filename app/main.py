04da56902ee46f95a6ab9c013e0b65062e65fd4e
ujjwal aditya <ujjwaladitya@ujjwals-MacBook-Air.local>
2025-08-25 00:26:51 +0530

    Add /healthz for UptimeRobot keep-alive

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse ,JSONResponse
 from fastapi.templating import Jinja2Templates
 import os
 
@@ -22,30 +22,13 @@ templates = Jinja2Templates(directory=TEMPLATE_DIR)
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+@app.get("/healthz")
+async def healthz():
+    return JSONResponse({"ok": True})
 
 
-# from fastapi import FastAPI, Request
-# from fastapi.staticfiles import StaticFiles
-# from fastapi.responses import HTMLResponse
-# from fastapi.templating import Jinja2Templates
-# import os
 
-# app = FastAPI(title="Portfolio - Ujjwal Aditya")
 
-# #Base directory (this is 'app' folder)
-# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-# #Static files
-# STATIC_DIR = os.path.join(BASE_DIR, "static")
-# app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
-
-# #Templates
-# TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
-# templates = Jinja2Templates(directory=TEMPLATE_DIR)
-
-# @app.get("/", response_class=HTMLResponse)
-# async def read_root(request: Request):
-#     return templates.TemplateResponse("index.html", {"request": request})
 
 
 
